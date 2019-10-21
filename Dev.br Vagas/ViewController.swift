a30fd95c6a42da643cd59dc4f6dbf275b4780a8f
Paulo José <paulocardosob@gmail.com>
2019-10-21 13:34:07 -0300

    Initial Commit

---
+++ Dev.br Vagas/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Dev.br Vagas
+//
+//  Created by Paulo José on 21/10/19.
+//  Copyright © 2019 Paulo José. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Dev.br VagasTests/Dev_br_VagasTests.swift
@@ -0,0 +1,34 @@
+//
+//  Dev_br_VagasTests.swift
+//  Dev.br VagasTests
+//
+//  Created by Paulo José on 21/10/19.
+//  Copyright © 2019 Paulo José. All rights reserved.
+//
+
+import XCTest
+@testable import Dev_br_Vagas
+
+class Dev_br_VagasTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
