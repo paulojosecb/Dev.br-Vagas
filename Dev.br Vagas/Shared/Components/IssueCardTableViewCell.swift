ed716169c1d7a08a5d6e5dc01829738965d9d644
Paulo José <paulocardosob@gmail.com>
2019-10-28 12:53:39 -0300

    Update IssueCardTableViewCell.swift

--- Dev.br Vagas/Shared/Components/IssueCardTableViewCell.swift
+++ Dev.br Vagas/Shared/Components/IssueCardTableViewCell.swift
@@ -10,7 +10,7 @@ import UIKit
 
 class IssueCardTableViewCell: UITableViewCell {
     
-    static var height: CGFloat = 120
+    static var height: CGFloat = 124
     
     var title: String? {
         didSet {
@@ -23,26 +23,26 @@ class IssueCardTableViewCell: UITableViewCell {
         }
     }
     
-    var state: String? {
-        didSet {
-            stateLabel.text = state == "open" ? "Aberta" : "Fechada"
-            stateLabel.backgroundColor = state == "open" ? .systemGreen : .systemRed
-            stateLabel.textColor = .white
-        }
-    }
+//    var state: String? {
+//        didSet {
+//            stateLabel.text = state == "open" ? "Aberta" : "Fechada"
+//            stateLabel.backgroundColor = state == "open" ? .systemGreen : .systemRed
+//            stateLabel.textColor = .white
+//        }
+//    }
     
-    var createdAt: String? {
-        didSet {
-            let formatter = DateFormatter()
-            formatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ssZ"
-            formatter.locale = Locale(identifier: "pt-BR")
-            let date = formatter.date(from: createdAt!) ?? Date()
-            let ptFormatter = DateFormatter()
-            ptFormatter.dateFormat = "dd/MM/yyyy"
-            let dateString = ptFormatter.string(from: date)
-            createdAtLabel.text = dateString
-        }
-    }
+//    var createdAt: String? {
+//        didSet {
+//            let formatter = DateFormatter()
+//            formatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ssZ"
+//            formatter.locale = Locale(identifier: "pt-BR")
+//            let date = formatter.date(from: createdAt!) ?? Date()
+//            let ptFormatter = DateFormatter()
+//            ptFormatter.dateFormat = "dd/MM/yyyy"
+//            let dateString = ptFormatter.string(from: date)
+//            createdAtLabel.text = dateString
+//        }
+//    }
 
     lazy var cardView: CardView = {
         let view = CardView()
@@ -53,49 +53,49 @@ class IssueCardTableViewCell: UITableViewCell {
     lazy var titleLabel: UILabel = {
         let label = UILabel()
         label.translatesAutoresizingMaskIntoConstraints = false
-        label.font = .systemFont(ofSize: 18, weight: .medium)
+        label.font = .title2
         label.text = "Título da Issue um tamanho suficiente para ser de duas linhas mas acho que da pra passar mais um pouco"
-        label.numberOfLines = 2
-        label.textColor = .white
+        label.numberOfLines = 0
+        label.textColor = .black76
         return label
     }()
     
-    lazy var createdAtLabel: UILabel = {
-        let label = UILabel()
-        label.translatesAutoresizingMaskIntoConstraints = false
-        label.font = .preferredFont(forTextStyle: .footnote)
-        label.text = "22/10/2019"
-        label.textColor = UIColor(white: 1, alpha: 0.8)
-        return label
-    }()
+//    lazy var createdAtLabel: UILabel = {
+//        let label = UILabel()
+//        label.translatesAutoresizingMaskIntoConstraints = false
+//        label.font = .preferredFont(forTextStyle: .footnote)
+//        label.text = "22/10/2019"
+//        label.textColor = UIColor(white: 1, alpha: 0.8)
+//        return label
+//    }()
     
     lazy var localLabel: UILabel = {
         let label = UILabel()
         label.translatesAutoresizingMaskIntoConstraints = false
-        label.font = .preferredFont(forTextStyle: .subheadline)
+        label.font = .subheadline
         label.text = "Rio de Janeiro/Remoto"
-        label.textColor = .white
+        label.textColor = .black40
         return label
     }()
         
-    lazy var stateLabel: UILabel = {
-       let label = UILabel()
-        label.translatesAutoresizingMaskIntoConstraints = false
-        label.text = "Aberta"
-        label.textAlignment = .center
-        label.font = .systemFont(ofSize: 18, weight: .bold)
-        label.layer.masksToBounds = true
-        label.layer.cornerRadius = 2
-        return label
-    }()
+//    lazy var stateLabel: UILabel = {
+//       let label = UILabel()
+//        label.translatesAutoresizingMaskIntoConstraints = false
+//        label.text = "Aberta"
+//        label.textAlignment = .center
+//        label.font = .systemFont(ofSize: 18, weight: .bold)
+//        label.layer.masksToBounds = true
+//        label.layer.cornerRadius = 2
+//        return label
+//    }()
     
     override func draw(_ rect: CGRect) {
-        cardView.setRoundedLayer(color: .darkBackground, radius: 10, shadowOppacity: 0.5, shadowRadius: 10)
+        cardView.setRoundedLayer(color: .white, radius: 10, shadowOppacity: 0.5, shadowRadius: 10)
     }
     
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
-        contentView.backgroundColor = .lightBackground
+        contentView.backgroundColor = .background
         setupView()
     }
     
@@ -119,29 +119,29 @@ extension IssueCardTableViewCell: CodeView {
         contentView.addSubview(cardView)
         contentView.addSubview(titleLabel)
         contentView.addSubview(localLabel)
-        contentView.addSubview(createdAtLabel)
+//        contentView.addSubview(createdAtLabel)
     }
     
     func setupConstraints() {
-        cardView.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 8).isActive = true
+        cardView.centerYAnchor.constraint(equalTo: self.centerYAnchor).isActive = true
         cardView.leftAnchor.constraint(equalTo: contentView.layoutMarginsGuide.leftAnchor).isActive = true
         cardView.rightAnchor.constraint(equalTo: contentView.layoutMarginsGuide.rightAnchor).isActive = true
-        cardView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -8).isActive = true
+        cardView.heightAnchor.constraint(equalToConstant: 103).isActive = true
         
         titleLabel.topAnchor.constraint(equalTo: cardView.topAnchor, constant: 8).isActive = true
         titleLabel.leftAnchor.constraint(equalTo: cardView.layoutMarginsGuide.leftAnchor).isActive = true
         titleLabel.rightAnchor.constraint(equalTo: cardView.layoutMarginsGuide.rightAnchor, constant: -8).isActive = true
-        titleLabel.heightAnchor.constraint(equalToConstant: 46).isActive = true
+        titleLabel.heightAnchor.constraint(equalToConstant: titleLabel.intrinsicContentSize.height).isActive = true
         
-        localLabel.bottomAnchor.constraint(equalTo: cardView.bottomAnchor, constant: -12).isActive = true
+        localLabel.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 0).isActive = true
         localLabel.leftAnchor.constraint(equalTo: titleLabel.leftAnchor).isActive = true
         localLabel.widthAnchor.constraint(equalTo: cardView.widthAnchor, multiplier: 0.75).isActive = true
         localLabel.heightAnchor.constraint(equalToConstant: localLabel.intrinsicContentSize.height).isActive = true
         
-        createdAtLabel.centerYAnchor.constraint(equalTo: localLabel.centerYAnchor).isActive = true
-        createdAtLabel.rightAnchor.constraint(equalTo: cardView.layoutMarginsGuide.rightAnchor).isActive = true
-        createdAtLabel.heightAnchor.constraint(equalToConstant: createdAtLabel.intrinsicContentSize.height).isActive = true
-        createdAtLabel.widthAnchor.constraint(equalToConstant: createdAtLabel.intrinsicContentSize.width).isActive = true
+//        createdAtLabel.centerYAnchor.constraint(equalTo: localLabel.centerYAnchor).isActive = true
+//        createdAtLabel.rightAnchor.constraint(equalTo: cardView.layoutMarginsGuide.rightAnchor).isActive = true
+//        createdAtLabel.heightAnchor.constraint(equalToConstant: createdAtLabel.intrinsicContentSize.height).isActive = true
+//        createdAtLabel.widthAnchor.constraint(equalToConstant: createdAtLabel.intrinsicContentSize.width).isActive = true
         
 //        stateLabel.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 0).isActive = true
 //        stateLabel.rightAnchor.constraint(equalTo: cardView.layoutMarginsGuide.rightAnchor).isActive = true
